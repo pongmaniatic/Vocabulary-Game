5e5372d4909a2d973f7507200e3d6f3f62316582
pongmaniatic <37563473+pongmaniatic@users.noreply.github.com>
2020-02-06 16:36:14 +0100

    normal update

--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class CardScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string WordSide1 = "Empty";
+    public string WordSide2 = "Empty";
+    public int Checks = 0;
+    public int Position = 0;
+    public bool CurrentSide = true;
+
+    // Start is called before the first frame update 
     void Start()
     {
-        
+        if (Position == 1)
+        {
+            transform.position = new Vector3(-5, 3, 0);
+        }
+        if (Position == 2)
+        {
+            transform.position = new Vector3(0, 3, 0);
+        }
+        if (Position == 3)
+        {
+            transform.position = new Vector3(5, 3, 0);
+        }
+        if (Position == 4)
+        {
+            transform.position = new Vector3(-5, 0, 0);
+        }
+        if (Position == 5)
+        {
+            transform.position = new Vector3(0, 0, 0);
+        }
+        if (Position == 6)
+        {
+            transform.position = new Vector3(5, 0, 0);
+        }
+        if (Position == 7)
+        {
+            transform.position = new Vector3(-5, -3, 0);
+        }
+        if (Position == 8)
+        {
+            transform.position = new Vector3(0, -3, 0);
+        }
+        if (Position == 9)
+        {
+            transform.position = new Vector3(5, -3, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
